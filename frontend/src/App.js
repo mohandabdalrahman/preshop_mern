76d120a6715daef8b8b40bd8e2320ea61e8ee8f2
Mohand Abdalrahman <mohand.abdalrahman@mondia.com>
2020-10-16 12:50:57 +0200

    finish header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header />
+      <main className="py-">
+        <Container>
+          <h1>Welcome to preshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
