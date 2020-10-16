092765251fe40ab52cac735c6163d8b1ee96d4b1
Mohand Abdalrahman <mohand.abdalrahman@mondia.com>
2020-10-16 15:08:37 +0200

    init backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const products = require('./data/products')
+const app = express()
+
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(product => product._id === req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`App listen on port ${PORT}`))
